b4bee66a9088e95b20a993a45995f3c870498b6e
Nick Waters <nickp60@users.noreply.github.com>
2023-02-21 11:02:34 -0500

    add more tests

--- tests/test.sh
+++ tests/test.sh
@@ -1,3 +1,5 @@
 # checking kreport2mpa.py
 
 python kreport2mpa.py  --report-file tests/test.report --output tests/output/test.report.mpa
+python kreport2krona.py  --report-file tests/test.report --output tests/output/test.report.krona
+python extract_kraken_reads.py  --report-file tests/test.report --output tests/output/test.report.reads
