d5d9521a7997661ad631d4a65f1f03b495f0d84e
Nick Waters <nickp60@users.noreply.github.com>
2023-02-21 10:54:53 -0500

    add test script; fix issue #54

--- kreport2mpa.py
+++ kreport2mpa.py
@@ -10,7 +10,7 @@
 #(at your option) any later version.
 
 #This program is distributed in the hope that it will be useful,
-#but WITHOUT ANY WARRANTY; without even the implied warranty of 
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
 #MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 #GNU General Public License for more details.
 
@@ -24,21 +24,21 @@
 #
 #This program reads in a Kraken report file and generates
 #an mpa-format (MetaPhlAn) style report. Each line represents
-#a possible taxon classification. The first column is lists the 
+#a possible taxon classification. The first column is lists the
 #domain, kingdom, phyla, etc, leading up to each taxon.
-#The levels are separated by the | delimiter, with the type of 
+#The levels are separated by the | delimiter, with the type of
 #level specified before each name with a single letter and underscore
-#(d_ for domain, k_ for kingdom, etc). 
-#The second column is the number of reads classified within 
+#(d_ for domain, k_ for kingdom, etc).
+#The second column is the number of reads classified within
 #that taxon's subtree.
 #
 #Input file:
 #   - Kraken report file generates from the kraken raw output file
 #Input Parameters to Specify [OPTIONAL]:
-#   - header_line = prints a header line in mpa-report 
+#   - header_line = prints a header line in mpa-report
 #       [Default: no header]
 #   - intermediate-ranks = includes non-traditional taxon levels
-#       (traditional levels: domain, kingdom, phylum, class, order, 
+#       (traditional levels: domain, kingdom, phylum, class, order,
 #       family, genus, species)
 #       [Default: no intermediate ranks]
 #Output file format (tab-delimited)
@@ -77,8 +77,8 @@ def process_kraken_report(curr_str):
     all_reads = int(split_str[1])
     #Extract relevant information
     try:
-        taxid = int(l_vals[-3]) 
-        level_type = l_vals[-2]
+        taxid = int(split_str[-3])
+        level_type = split_str[-2]
         map_kuniq = {'species':'S', 'genus':'G','family':'F',
             'order':'O','class':'C','phylum':'P','superkingdom':'D',
             'kingdom':'K'}
@@ -87,9 +87,9 @@ def process_kraken_report(curr_str):
         else:
             level_type = map_kuniq[level_type]
     except ValueError:
-        taxid = int(l_vals[-2])
+        taxid = int(split_str[-2])
         level_type = split_str[-3]
-    #Get name and spaces 
+    #Get name and spaces
     spaces = 0
     name = split_str[-1]
     for char in name:
@@ -111,9 +111,9 @@ def main():
         dest='r_file', help='Input kraken report file for converting')
     parser.add_argument('-o', '--output', required=True,
         dest='o_file', help='Output mpa-report file name')
-    parser.add_argument('--display-header', action='store_true', 
+    parser.add_argument('--display-header', action='store_true',
         dest='add_header', default=False, required=False,
-        help='Include header [Kraken report filename] in mpa-report file [default: no header]') 
+        help='Include header [Kraken report filename] in mpa-report file [default: no header]')
     parser.add_argument('--read_count', action='store_true',
         dest='use_reads', default=True, required=False,
         help='Use read count for output [default]')
@@ -128,27 +128,27 @@ def main():
         help='Do not include non-traditional taxonomic ranks in output [default]')
     args=parser.parse_args()
 
-    #Process report file and output 
-    curr_path = [] 
+    #Process report file and output
+    curr_path = []
     prev_lvl_num = -1
     r_file = open(args.r_file, 'r')
     o_file = open(args.o_file, 'w')
     #Print header
     if args.add_header:
         o_file.write("#Classification\t" + os.path.basename(args.r_file) + "\n")
-    
-    #Read through report file 
+
+    #Read through report file
     main_lvls = ['R','K','D','P','C','O','F','G','S']
     for line in r_file:
         report_vals = process_kraken_report(line)
         #If header line, skip
-        if len(report_vals) < 5: 
+        if len(report_vals) < 5:
             continue
-        #Get relevant information from the line 
+        #Get relevant information from the line
         [name, level_num, level_type, all_reads, percents] = report_vals
         if level_type == 'U':
             continue
-        #Create level name 
+        #Create level name
         if level_type not in main_lvls:
             level_type = "x"
         elif level_type == "K":
@@ -171,7 +171,7 @@ def main():
                 #Print all ancestors of current level followed by |
                 for string in curr_path:
                     if (string[0] == "x" and args.x_include) or string[0] != "x":
-                        if string[0] != "r": 
+                        if string[0] != "r":
                             o_file.write(string + "|")
                 #Print final level and then number of reads
                 if args.use_reads:
---
+++ tests/test.sh
@@ -0,0 +1,3 @@
+# checking kreport2mpa.py
+
+python kreport2mpa.py  --report-file tests/test.report --output tests/output/test.report.mpa
